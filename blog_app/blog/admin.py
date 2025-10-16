24e9c5f74eb2c0b50c2a0da0f00af4fd20f96366
Mr-Yang03 <duongphucthang2003@gmail.com>
2025-10-16 16:43:24 +0700

    Register models into Django admin.

--- blog_app/blog/admin.py
+++ blog_app/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post, Comment, Category, Tag, Like
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Like)
--- blog_app/notifications/admin.py
+++ blog_app/notifications/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Notification, EmailLog
 
 # Register your models here.
+admin.site.register(Notification)
+admin.site.register(EmailLog)
